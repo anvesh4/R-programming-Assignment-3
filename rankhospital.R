450cc302119de2e5c8efd9b2425da88597604a14
Anvesh <anveshvispotin@gmail.com>
2016-12-14 14:58:00 -0500

    3 Ranking hospitals by outcome in a state

---
+++ rankhospital.R
@@ -0,0 +1,61 @@
+rankhospital <- function(state, outcome,num="best") {
+    
+    ## Read outcome data    
+    data <- read.csv("outcome-of-care-measures.csv",na.strings="Not Available",stringsAsFactors=FALSE)
+    
+    ## subset the three columns
+    outcomevector <- c("heart attack"=11, "heart failure"=17, "pneumonia"= 23)
+    
+    ##check the outcome is valid
+    
+    if(!(outcome %in%  c("heart attack", "heart failure", "pneumonia"))) {
+        stop("invalid outcome")
+    }
+    
+    
+    df.NAs <- data[,c(2,7,outcomevector[outcome])]	
+    names(df.NAs)<-c("hospital","state","outcome")
+    #print(head(df.NAs))
+    
+    
+    
+    ## Check that state and outcome are valid
+    
+    if(!(state %in% df.NAs$state)) {
+        stop("invalid state")
+    }
+    
+    ##remove NAs
+    nonarrangeddf<-na.omit(df.NAs)
+    #print(head(nonarrangeddf))
+    
+    ## Order by state then outcome then hospital name
+    library(plyr)
+    df <- arrange(nonarrangeddf,state,outcome,hospital)
+    #print(head(df))
+    
+    ##Return hospital name in that state with lowest 30-day death rate
+    
+    subset.state <- df[df$state==state,]
+    #print(subset.state)
+    
+    if(num == "best") {
+    
+    subset.state$hospital[1]
+    # print(subset.state[1,1])  this too works.but for some reason both of them 
+                                #together is not working
+        
+    }
+    else if(num == "worst") {
+        
+        #subset.state[nrow(subset.state$hospital),1] this ain't working.why?
+        subset.state[nrow(subset.state),1]
+        
+    }
+    else {
+        subset.state[num,1]
+        #subset.state$hospital[num] this too works!!!
+    }
+    
+    
+}
